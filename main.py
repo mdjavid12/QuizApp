095fdaf3ebb063a50144298e017daa64a966c372
Mohmmed Javid <mohmmedjavid12@gmail.com>
2025-11-23 14:11:25 +0530

    Refactor admin user creation logic in main.py

--- main.py
+++ main.py
@@ -11,27 +11,24 @@ app = FastAPI()
 
 pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Include routers
+
 app.include_router(auth_router)
 app.include_router(questions_router)
 app.include_router(quiz_router)
 
-# --------------------------------------
-# Create database tables
-# --------------------------------------
+
+
 Base.metadata.create_all(bind=engine)
 
-# --------------------------------------
-# Create default admin user on startup
-# --------------------------------------
+
 ADMIN_EMAIL = "admin@example.com"
-ADMIN_PASSWORD = "admin123"  # you can change this
+ADMIN_PASSWORD = "admin123"  
 
 def create_admin_user():
     db: Session = next(get_db())
     admin = db.query(User).filter(User.email == ADMIN_EMAIL).first()
     if not admin:
-        hashed = pwd.hash(ADMIN_PASSWORD[:72])  # safe for bcrypt
+        hashed = pwd.hash(ADMIN_PASSWORD[:72])  
         admin = User(email=ADMIN_EMAIL, password=hashed, is_admin=True)
         db.add(admin)
         db.commit()
